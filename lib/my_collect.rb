1f411b4fdda2d93eafdd2756385347bca1513983
Scott  Keplinger <skeplin17@gmail.com>
2020-05-07 22:22:24 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect (collection)
+  if block_given?
+  i = 0
+  collection_a = []
+    while i < collection.length
+    collection_a.push(yield collection[i])
+     i += 1
+  end
+  collection_a
+ else
+end
+end
